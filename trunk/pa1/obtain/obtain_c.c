c566ab31632487a24a16b59796b4975df2b24b99
ravi.m85@gmail.com <ravi.m85@gmail.com@0177215c-dcb8-4386-07b5-442ffd92dceb>
2011-02-23 14:03:24 +0000

    works better

--- trunk/pa1/obtain/obtain_c.c
+++ trunk/pa1/obtain/obtain_c.c
@@ -5,93 +5,95 @@
 
 extern __thread int errno;
 
-int get_file(char *host, char *name)
-{
-    CLIENT *clnt;
-    int total_bytes = 0, write_bytes;
-    readfile_res *result;
-    request req;
-    FILE *file;
-    int flag = 1;
-    req.name = name;
-    req.seek_bytes = 0;
-    /*
-     * Create client handle used for calling FTPPROG on
-     * the server designated on the command line. Use
-     * the tcp protocol when contacting the server.
-     */
-    clnt = clnt_create(host, OBTAINPROG, OBTAINVER, "tcp");
-    if (clnt == NULL) {
-        /*
-         * Couldn't establish connection with server.
-         * Print error message and stop.
-         */
-         clnt_pcreateerror(host);
-         exit(1);
-    }
+int get_file(char *host, char *name) {
 
-    file = fopen("/tmp/name", "wb");
+	CLIENT *clnt;
+	int total_bytes = 0, write_bytes;
+	readfile_res *result;
+	request req;
+	FILE *file;
+	int flag = 1;
+	req.name = name;
+	req.seek_bytes = 0;
 
-    /*
-     * Call the remote procedure readdir on the server
-     */
-    while (1) {
-        req.seek_bytes = total_bytes;
-        result = obtain_1(&req, clnt);
-        if (result == NULL) {
-            /*
-             * An RPC error occurred while calling the server.
-             * Print error message and stop.
-             */
-            clnt_perror(clnt, host);
-            exit(1);
-        }
+	/*
+	* Create client handle used for calling FTPPROG on
+	* the server designated on the command line. Use
+	* the tcp protocol when contacting the server.
+	*/
+	clnt = clnt_create(host, OBTAINPROG, OBTAINVER, "tcp");
+	if (clnt == NULL) {
+	/*
+	* establish connection failed with the server.
+	*/
+		clnt_pcreateerror(host);
+	        exit(1);
+   	}
+
+	//currently hardcoded to this as I am testing it on the same box....
+        file = fopen("/tmp/name", "wb");
 
         /*
-         * Okay, we successfully called the remote procedure.
-         */
-        if (result->errno != 0) {
-            /*
-             * A remote system error occurred.
-             * Print error message and stop.
-             */
-            errno = result->errno;
-            perror(name);
-            exit(1);
-        }
-        /*
-         * Successfully got a chunk of the file.
-         * Write into our local file.
-         */
-	//printf("\n read %d \t %s", result->readfile_res_u.chunk.bytes,result->readfile_res_u.chunk.data);
-        write_bytes = fwrite(result->readfile_res_u.chunk.data, 1, result->readfile_res_u.chunk.bytes, file);
+        * Call the remote procedure readdir on the server
+        */
+        while (1) {
+		
+    	        req.seek_bytes = total_bytes;
+	        result = obtain_1(&req, clnt);
+	        if (result == NULL) {
+        		/*
+	                * An RPC error occurred while calling the server.
+              		* Print error message and stop.
+	                */
+           		clnt_perror(clnt, host);
+		        exit(1);
+       		}
 
-	//printf("\n  write %d \t %s", result->readfile_res_u.chunk.bytes, result->readfile_res_u.chunk.data);
+	        /*
+ 	        * Okay, we successfully called the remote procedure.
+         	*/
+	        if (result->errno != 0) {
+          		/*
+	         	* A remote system error occurred.
+	   		* Print error message and stop.
+	                */
+        		errno = result->errno;
+		        perror(name);
+		        exit(1);
+        	}
+        	/*
+	        * Successfully got a chunk of the file.
+	        * Write into our local file.
+        	*/
+
+		//printf("\n read %d \t %s", result->readfile_res_u.chunk.bytes,result->readfile_res_u.chunk.data);
+        	write_bytes = fwrite(result->readfile_res_u.chunk.data, 1, result->readfile_res_u.chunk.bytes, file);
+
+		//printf("\n  write %d \t %s", result->readfile_res_u.chunk.bytes, result->readfile_res_u.chunk.data);
 
 
-	// lets print the total number of bytes read and written in each cycle
-	printf(" \n read %d written %d ",result->readfile_res_u.chunk.bytes,write_bytes);        
-        total_bytes += result->readfile_res_u.chunk.bytes;
-	flag = 0;
-        if (result->readfile_res_u.chunk.bytes < SIZE ) 
-            break;
-    }
+		// lets print the total number of bytes read and written in each cycle
+		printf(" \n read %d written %d ",result->readfile_res_u.chunk.bytes,write_bytes);        
+        	total_bytes += result->readfile_res_u.chunk.bytes;
+		if (result->readfile_res_u.chunk.bytes < SIZE ) 
+                break;
+	}
 
-    fclose(file);
+    	fclose(file);
 
-    return 0;
+	return 0;
 }
 
 
 int main(int argc, char *argv[])
 {
-   int result;
+	int result;
 
-   if (argc != 3) {
-        fprintf(stderr, "usage: %s host filepath\n", argv[0]);
-        exit(1);
-   }
-   result = get_file(argv[1], argv[2]);
+        if (argc != 3) {
+        	fprintf(stderr, "usage: %s host filepath\n", argv[0]);
+        	exit(1);
+   	}
+        result = get_file(argv[1], argv[2]);
 
-   return 0;
+        return 0;
 }
--- trunk/pa1/obtain/obtain_s.c
+++ trunk/pa1/obtain/obtain_s.c
@@ -4,29 +4,24 @@
 
 extern __thread int errno;
 
-readfile_res* obtain_1_svc(request *req, struct svc_req *rqstp)
-{
-    FILE *file;
-    int bytes;
-    static readfile_res res;
-    static char data[SIZE];
+readfile_res* obtain_1_svc(request *req, struct svc_req *rqstp) {
+	FILE *file;
+	int bytes;
+	static readfile_res res;
+        static char data[SIZE];
 
-    file = fopen(req->name, "rb");
-	printf("hello ");
-    if (file == NULL) {
-        res.errno = errno;
+        file = fopen(req->name, "rb");
+        if (file == NULL) {
+        	res.errno = errno;
+	        return (&res);
+        }
+
+        fseek (file, req->seek_bytes, SEEK_SET);
+        bytes = fread(data, 1, SIZE, file);
+        res.readfile_res_u.chunk.data = data;
+        res.readfile_res_u.chunk.bytes = bytes;
+
+        res.errno = 0;
+        fclose(file);
         return (&res);
-    }
-
-    fseek (file, req->seek_bytes, SEEK_SET);
-    bytes = fread(data, 1, SIZE, file);
-    res.readfile_res_u.chunk.data = data;
-    res.readfile_res_u.chunk.bytes = bytes;
-
-    /*
-     * Return the result
-     */
-    res.errno = 0;
-    fclose(file);
-    return (&res);
 }
