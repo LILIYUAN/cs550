3f25628489cc21b5ed91f662524989a238bcae82
ravi.m85@gmail.com <ravi.m85@gmail.com@0177215c-dcb8-4386-07b5-442ffd92dceb>
2011-03-19 04:48:10 +0000

    base of pa2

---
+++ trunk/pa2/test.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/ksh
+
+# This test script is used to make multiple calls fetch the files
+# from the peer servers. We need to pass the index-server, file-name
+# and the dest-dir to download / transfer the file on the local
+# machine. Currently it is set to 1000 iterations. 1000 sequential
+# calls are made to the peer servers to transfer the file.
+
+
+ i=0
+
+while [ $i -lt 1000 ]
+do
+
+    ./obtain_client -f $1 $2 $3
+    i=$i+1
+done
+
