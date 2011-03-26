cdb24d2c1400a5c0476d9755279fadfd2e7d8849
ravi.m85 <ravi.m85@gmail.com>
2011-03-26 15:28:17 +0000

    removing makeall.sh

--- pa2/makeall.sh
+++
@@ -1,7 +0,0 @@
-#
-# This can be used to compile all the code.
-# - obtain_server   : The peer server executable
-# - obtain_client   : The peer client which the user can use to fetch the files
-#
- 
-make -f Makefile.obtain
