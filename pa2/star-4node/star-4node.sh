e2f2a2325d6851bbbfe4935f69dcbab5eed91893
ravi.m85 <ravi.m85@gmail.com>
2011-03-27 06:13:15 +0000

    upfated the scripts

--- pa2/star-4node/star-4node.sh
+++ pa2/star-4node/star-4node.sh
@@ -2,11 +2,11 @@
 
 BASEDIR="~/cs550/pa2/"
 ssh compute-0-0 "rm -rf /tmp/indsvr"
-ssh compute-0-0 "$BASEDIR/obtain_server compute-0-0 $BASEDIR/star-4node/compute-0-0.peers $BASEDIR/data/share0" &
+ssh compute-0-0 "$BASEDIR/obtain_server $BASEDIR/star-4node/compute-0-0.peers $BASEDIR/data/share0" &
 ssh compute-0-1 "rm -rf /tmp/indsvr"
-ssh compute-0-1 "$BASEDIR/obtain_server compute-0-1 $BASEDIR/star-4node/compute-0-1.peers $BASEDIR/data/share1" &
+ssh compute-0-1 "$BASEDIR/obtain_server $BASEDIR/star-4node/compute-0-1.peers $BASEDIR/data/share1" &
 ssh compute-0-2"rm -rf /tmp/indsvr"
-ssh compute-0-2 "$BASEDIR/obtain_server compute-0-2 $BASEDIR/star-4node/compute-0-2.peers $BASEDIR/data/share2" &
+ssh compute-0-2 "$BASEDIR/obtain_server $BASEDIR/star-4node/compute-0-2.peers $BASEDIR/data/share2" &
 ssh compute-0-3 "rm -rf /tmp/indsvr"
-ssh compute-0-3 "$BASEDIR/obtain_server compute-0-3 $BASEDIR/star-4node/compute-0-3.peers $BASEDIR/data/share3" &
+ssh compute-0-3 "$BASEDIR/obtain_server $BASEDIR/star-4node/compute-0-3.peers $BASEDIR/data/share3" &
 
