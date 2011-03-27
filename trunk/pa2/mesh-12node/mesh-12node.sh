b70e21aa383d36c68d35ca844839aa8264262556
ravi.m85@gmail.com <ravi.m85@gmail.com@0177215c-dcb8-4386-07b5-442ffd92dceb>
2011-03-27 09:04:19 +0000

    more

--- trunk/pa2/mesh-12node/mesh-12node.sh
+++ trunk/pa2/mesh-12node/mesh-12node.sh
@@ -5,7 +5,7 @@ ssh compute-0-0 "rm -rf /tmp/indsvr"
 ssh compute-0-0 "$BASEDIR/obtain_server $BASEDIR/mesh-12node/compute-0-0.peers $BASEDIR/data/share0" &
 ssh compute-0-1 "rm -rf /tmp/indsvr"
 ssh compute-0-1 "$BASEDIR/obtain_server $BASEDIR/mesh-12node/compute-0-1.peers $BASEDIR/data/share1" &
-ssh compute-0-2"rm -rf /tmp/indsvr"
+ssh compute-0-2 "rm -rf /tmp/indsvr"
 ssh compute-0-2 "$BASEDIR/obtain_server $BASEDIR/mesh-12node/compute-0-2.peers $BASEDIR/data/share2" &
 ssh compute-0-3 "rm -rf /tmp/indsvr"
 ssh compute-0-3 "$BASEDIR/obtain_server $BASEDIR/mesh-12node/compute-0-3.peers $BASEDIR/data/share3" &
