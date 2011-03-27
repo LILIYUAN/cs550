8360e0e07d4f97f437a3c1b582641886db44a693
ravi.m85@gmail.com <ravi.m85@gmail.com@0177215c-dcb8-4386-07b5-442ffd92dceb>
2011-03-27 05:00:14 +0000

    fixed the script

--- trunk/pa2/star-10node/star-10node.sh
+++ trunk/pa2/star-10node/star-10node.sh
@@ -10,14 +10,14 @@ ssh compute-0-2 "$BASEDIR/obtain_server compute-0-2 $BASEDIR/star-10node/compute
 ssh compute-0-3 "rm -rf /tmp/indsvr"
 ssh compute-0-3 "$BASEDIR/obtain_server compute-0-3 $BASEDIR/star-10node/compute-0-3.peers $BASEDIR/data/share3" &
 ssh compute-0-10 "rm -rf /tmp/indsvr"
-ssh compute-0-10 "$BASEDIR/obtain_server compute-0-10 $BASEDIR/star-10node/compute-0-10.peers $BASEDIR/data/share3" &
+ssh compute-0-10 "$BASEDIR/obtain_server compute-0-10 $BASEDIR/star-10node/compute-0-10.peers $BASEDIR/data/share4" &
 ssh compute-0-5 "rm -rf /tmp/indsvr"
-ssh compute-0-5 "$BASEDIR/obtain_server compute-0-5 $BASEDIR/star-10node/compute-0-5.peers $BASEDIR/data/share3" &
+ssh compute-0-5 "$BASEDIR/obtain_server compute-0-5 $BASEDIR/star-10node/compute-0-5.peers $BASEDIR/data/share5" &
 ssh compute-0-6 "rm -rf /tmp/indsvr"
-ssh compute-0-6 "$BASEDIR/obtain_server compute-0-6 $BASEDIR/star-10node/compute-0-6.peers $BASEDIR/data/share3" &
+ssh compute-0-6 "$BASEDIR/obtain_server compute-0-6 $BASEDIR/star-10node/compute-0-6.peers $BASEDIR/data/share6" &
 ssh compute-0-7 "rm -rf /tmp/indsvr"
-ssh compute-0-7 "$BASEDIR/obtain_server compute-0-7 $BASEDIR/star-10node/compute-0-7.peers $BASEDIR/data/share3" &
+ssh compute-0-7 "$BASEDIR/obtain_server compute-0-7 $BASEDIR/star-10node/compute-0-7.peers $BASEDIR/data/share7" &
 ssh compute-0-8 "rm -rf /tmp/indsvr"
-ssh compute-0-8 "$BASEDIR/obtain_server compute-0-8 $BASEDIR/star-10node/compute-0-8.peers $BASEDIR/data/share3" &
+ssh compute-0-8 "$BASEDIR/obtain_server compute-0-8 $BASEDIR/star-10node/compute-0-8.peers $BASEDIR/data/share8" &
 ssh compute-0-9 "rm -rf /tmp/indsvr"
-ssh compute-0-9 "$BASEDIR/obtain_server compute-0-9 $BASEDIR/star-10node/compute-0-9.peers $BASEDIR/data/share3" &
+ssh compute-0-9 "$BASEDIR/obtain_server compute-0-9 $BASEDIR/star-10node/compute-0-9.peers $BASEDIR/data/share9" &
