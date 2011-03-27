69bf306429b9e131f089f144031b097a9428d259
ravi.m85 <ravi.m85@gmail.com>
2011-03-27 04:55:12 +0000

    added clear script

---
+++ pa2/clear_all.sh
@@ -0,0 +1,14 @@
+#!/bin/ksh
+
+BASEDIR="~/cs550/pa2/"
+ssh compute-0-0 "pkill obtain_server"
+ssh compute-0-1 "pkill obtain_server"
+ssh compute-0-2 "pkill obtain_server"
+ssh compute-0-3 "pkill obtain_server"
+ssh compute-0-5 "pkill obtain_server"
+ssh compute-0-6 "pkill obtain_server"
+ssh compute-0-7 "pkill obtain_server"
+ssh compute-0-8 "pkill obtain_server"
+ssh compute-0-9 "pkill obtain_server"
+ssh compute-0-10 "pkill obtain_server"
+ssh compute-0-11 "pkill obtain_server"
