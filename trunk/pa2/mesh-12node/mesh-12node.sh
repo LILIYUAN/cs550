c9f565becae4109009acc5b1999d4c1eb7d794bd
ravi.m85@gmail.com <ravi.m85@gmail.com@0177215c-dcb8-4386-07b5-442ffd92dceb>
2011-03-27 06:13:15 +0000

    upfated the scripts

--- trunk/pa2/mesh-12node/mesh-12node.sh
+++ trunk/pa2/mesh-12node/mesh-12node.sh
@@ -2,26 +2,26 @@
 
 BASEDIR="~/cs550/pa2/"
 ssh compute-0-0 "rm -rf /tmp/indsvr"
-ssh compute-0-0 "$BASEDIR/obtain_server compute-0-0 $BASEDIR/mesh-10node/compute-0-0.peers $BASEDIR/data/share0" &
+ssh compute-0-0 "$BASEDIR/obtain_server $BASEDIR/mesh-12node/compute-0-0.peers $BASEDIR/data/share0" &
 ssh compute-0-1 "rm -rf /tmp/indsvr"
-ssh compute-0-1 "$BASEDIR/obtain_server compute-0-1 $BASEDIR/mesh-10node/compute-0-1.peers $BASEDIR/data/share1" &
+ssh compute-0-1 "$BASEDIR/obtain_server $BASEDIR/mesh-12node/compute-0-1.peers $BASEDIR/data/share1" &
 ssh compute-0-2"rm -rf /tmp/indsvr"
-ssh compute-0-2 "$BASEDIR/obtain_server compute-0-2 $BASEDIR/mesh-10node/compute-0-2.peers $BASEDIR/data/share2" &
+ssh compute-0-2 "$BASEDIR/obtain_server $BASEDIR/mesh-12node/compute-0-2.peers $BASEDIR/data/share2" &
 ssh compute-0-3 "rm -rf /tmp/indsvr"
-ssh compute-0-3 "$BASEDIR/obtain_server compute-0-3 $BASEDIR/mesh-10node/compute-0-3.peers $BASEDIR/data/share3" &
+ssh compute-0-3 "$BASEDIR/obtain_server $BASEDIR/mesh-12node/compute-0-3.peers $BASEDIR/data/share3" &
 ssh compute-0-12 "rm -rf /tmp/indsvr"
-ssh compute-0-12 "$BASEDIR/obtain_server compute-0-10 $BASEDIR/mesh-10node/compute-0-12.peers $BASEDIR/data/share12" &
+ssh compute-0-12 "$BASEDIR/obtain_server $BASEDIR/mesh-12node/compute-0-12.peers $BASEDIR/data/share12" &
 ssh compute-0-5 "rm -rf /tmp/indsvr"
-ssh compute-0-5 "$BASEDIR/obtain_server compute-0-5 $BASEDIR/mesh-10node/compute-0-5.peers $BASEDIR/data/share5" &
+ssh compute-0-5 "$BASEDIR/obtain_server $BASEDIR/mesh-12node/compute-0-5.peers $BASEDIR/data/share5" &
 ssh compute-0-6 "rm -rf /tmp/indsvr"
-ssh compute-0-6 "$BASEDIR/obtain_server compute-0-6 $BASEDIR/mesh-10node/compute-0-6.peers $BASEDIR/data/share6" &
+ssh compute-0-6 "$BASEDIR/obtain_server $BASEDIR/mesh-12node/compute-0-6.peers $BASEDIR/data/share6" &
 ssh compute-0-7 "rm -rf /tmp/indsvr"
-ssh compute-0-7 "$BASEDIR/obtain_server compute-0-7 $BASEDIR/mesh-10node/compute-0-7.peers $BASEDIR/data/share7" &
+ssh compute-0-7 "$BASEDIR/obtain_server $BASEDIR/mesh-12node/compute-0-7.peers $BASEDIR/data/share7" &
 ssh compute-0-8 "rm -rf /tmp/indsvr"
-ssh compute-0-8 "$BASEDIR/obtain_server compute-0-8 $BASEDIR/mesh-10node/compute-0-8.peers $BASEDIR/data/share8" &
+ssh compute-0-8 "$BASEDIR/obtain_server $BASEDIR/mesh-12node/compute-0-8.peers $BASEDIR/data/share8" &
 ssh compute-0-9 "rm -rf /tmp/indsvr"
-ssh compute-0-9 "$BASEDIR/obtain_server compute-0-9 $BASEDIR/mesh-10node/compute-0-9.peers $BASEDIR/data/share9" &
+ssh compute-0-9 "$BASEDIR/obtain_server $BASEDIR/mesh-12node/compute-0-9.peers $BASEDIR/data/share9" &
 ssh compute-0-10 "rm -rf /tmp/indsvr"
-ssh compute-0-10 "$BASEDIR/obtain_server compute-0-10 $BASEDIR/mesh-10node/compute-0-10.peers $BASEDIR/data/share10" &
+ssh compute-0-10 "$BASEDIR/obtain_server $BASEDIR/mesh-12node/compute-0-10.peers $BASEDIR/data/share10" &
 ssh compute-0-11 "rm -rf /tmp/indsvr"
-ssh compute-0-11 "$BASEDIR/obtain_server compute-0-11 $BASEDIR/mesh-10node/compute-0-11.peers $BASEDIR/data/share11" &
+ssh compute-0-11 "$BASEDIR/obtain_server $BASEDIR/mesh-12node/compute-0-11.peers $BASEDIR/data/share11" &
--- trunk/pa2/start-mesh-4.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/ksh
-
-ssh compute-0-0 "~/trunk/pa2/obtain_server compute-0-0 ~/trunk/pa2/star-4node ~/trunk/pa2/data/share0"
