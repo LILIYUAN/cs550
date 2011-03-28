2b0ac36c47e6cd589a3c4cfd129956bebf45c57f
ravi.m85 <ravi.m85@gmail.com>
2011-03-28 11:29:59 +0000

    cleaning up docs and commments

--- pa2/test.sh
+++ pa2/test.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/ksh
 
 # This test script is used to make multiple calls fetch the files
-# from the peer servers. We need to pass the local host name from 
-# where the search should start, file-name
+# from the peer servers. We need to pass the file name 
 # and the dest-dir to download / transfer the file on the local
-# machine. Currently it is set to 1000 iterations. 1000 sequential
+# machine. Currently it is set to 200 iterations. 200 sequential
 # calls are made to the peer servers to transfer the file.
+
+# -f option is used to pull the file from the first peer 
+# serving the file.
 
 
  i=0
