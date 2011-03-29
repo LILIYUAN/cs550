e6d3cd77bf256f0d82ad566a4f5ca7e5b5c9dc45
sanjeev.bagewadi <sanjeev.bagewadi@gmail.com>
2011-03-29 16:53:54 +0000

    Modified to log the debug messages to a file

--- pa2/star-4node/star-4node.sh
+++ pa2/star-4node/star-4node.sh
@@ -2,8 +2,8 @@
 
 BASEDIR="~/cs550/pa2/"
 
-ssh compute-0-0 "$BASEDIR/obtain_server $BASEDIR/star-4node/compute-0-0.peers $BASEDIR/data/share0" &
-ssh compute-0-1 "$BASEDIR/obtain_server $BASEDIR/star-4node/compute-0-1.peers $BASEDIR/data/share1" &
-ssh compute-0-2 "$BASEDIR/obtain_server $BASEDIR/star-4node/compute-0-2.peers $BASEDIR/data/share2" &
-ssh compute-0-3 "$BASEDIR/obtain_server $BASEDIR/star-4node/compute-0-3.peers $BASEDIR/data/share3" &
+ssh compute-0-0 "$BASEDIR/obtain_server $BASEDIR/star-4node/compute-0-0.peers $BASEDIR/data/share0 > /tmp/server0.log" &
+ssh compute-0-1 "$BASEDIR/obtain_server $BASEDIR/star-4node/compute-0-1.peers $BASEDIR/data/share1 > /tmp/server1.log" &
+ssh compute-0-2 "$BASEDIR/obtain_server $BASEDIR/star-4node/compute-0-2.peers $BASEDIR/data/share2 > /tmp/server2.log" &
+ssh compute-0-3 "$BASEDIR/obtain_server $BASEDIR/star-4node/compute-0-3.peers $BASEDIR/data/share3 > /tmp/server3.log" &
 
