6586bda32708d3884456215430c2faec5fddb13b
sanjeev.bagewadi@gmail.com <sanjeev.bagewadi@gmail.com@0177215c-dcb8-4386-07b5-442ffd92dceb>
2011-03-29 19:33:23 +0000

    Added logging

--- trunk/pa2/star-10node/star-10node.sh
+++ trunk/pa2/star-10node/star-10node.sh
@@ -2,13 +2,13 @@
 
 BASEDIR="~/cs550/pa2/"
 
-ssh compute-0-0 "$BASEDIR/obtain_server $BASEDIR/star-10node/compute-0-0.peers $BASEDIR/data/share0" &
-ssh compute-0-1 "$BASEDIR/obtain_server $BASEDIR/star-10node/compute-0-1.peers $BASEDIR/data/share1" &
-ssh compute-0-2 "$BASEDIR/obtain_server $BASEDIR/star-10node/compute-0-2.peers $BASEDIR/data/share2" &
-ssh compute-0-3 "$BASEDIR/obtain_server $BASEDIR/star-10node/compute-0-3.peers $BASEDIR/data/share3" &
-ssh compute-0-10 "$BASEDIR/obtain_server $BASEDIR/star-10node/compute-0-10.peers $BASEDIR/data/share10" &
-ssh compute-0-5 "$BASEDIR/obtain_server $BASEDIR/star-10node/compute-0-5.peers $BASEDIR/data/share5" &
-ssh compute-0-6 "$BASEDIR/obtain_server $BASEDIR/star-10node/compute-0-6.peers $BASEDIR/data/share6" &
-ssh compute-0-7 "$BASEDIR/obtain_server $BASEDIR/star-10node/compute-0-7.peers $BASEDIR/data/share7" &
-ssh compute-0-8 "$BASEDIR/obtain_server $BASEDIR/star-10node/compute-0-8.peers $BASEDIR/data/share8" &
-ssh compute-0-9 "$BASEDIR/obtain_server $BASEDIR/star-10node/compute-0-9.peers $BASEDIR/data/share9" &
+ssh compute-0-0 "$BASEDIR/obtain_server $BASEDIR/star-10node/compute-0-0.peers $BASEDIR/data/share0 > /tmp/star-10-0.log" &
+ssh compute-0-1 "$BASEDIR/obtain_server $BASEDIR/star-10node/compute-0-1.peers $BASEDIR/data/share1 > /tmp/star-10-1.log" &
+ssh compute-0-2 "$BASEDIR/obtain_server $BASEDIR/star-10node/compute-0-2.peers $BASEDIR/data/share2 > /tmp/star-10-2.log" &
+ssh compute-0-3 "$BASEDIR/obtain_server $BASEDIR/star-10node/compute-0-3.peers $BASEDIR/data/share3 > /tmp/star-10-3.log" &
+ssh compute-0-10 "$BASEDIR/obtain_server $BASEDIR/star-10node/compute-0-10.peers $BASEDIR/data/share10 > /tmp/star-10-4.log" &
+ssh compute-0-5 "$BASEDIR/obtain_server $BASEDIR/star-10node/compute-0-5.peers $BASEDIR/data/share5 > /tmp/star-10-5.log" &
+ssh compute-0-6 "$BASEDIR/obtain_server $BASEDIR/star-10node/compute-0-6.peers $BASEDIR/data/share6 > /tmp/star-10-6.log" &
+ssh compute-0-7 "$BASEDIR/obtain_server $BASEDIR/star-10node/compute-0-7.peers $BASEDIR/data/share7 > /tmp/star-10-7.log" &
+ssh compute-0-8 "$BASEDIR/obtain_server $BASEDIR/star-10node/compute-0-8.peers $BASEDIR/data/share8 > /tmp/star-10-8.log" &
+ssh compute-0-9 "$BASEDIR/obtain_server $BASEDIR/star-10node/compute-0-9.peers $BASEDIR/data/share9 > /tmp/star-10-9.log" &
