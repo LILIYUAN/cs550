a2431b6bb16a9a4d196cd0f8096a8e8cebd394a0
ravi.m85 <ravi.m85@gmail.com>
2011-05-01 14:49:26 +0000

    updated script

--- pa3/relay-test/relay-test.sh
+++ pa3/relay-test/relay-test.sh
@@ -1,6 +1,6 @@
 #!/bin/ksh
 
-BASEDIR="~/cs550/pa2/"
+BASEDIR="~/cs550/pa3/"
 
 ssh compute-0-0 "$BASEDIR/obtain_server $BASEDIR/relay-test/compute-0-0.peers $BASEDIR/data/share0" &
 ssh compute-0-1 "$BASEDIR/obtain_server $BASEDIR/relay-test/compute-0-1.peers $BASEDIR/data/share1" &
