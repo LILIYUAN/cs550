e342f29fed2d93123a31cb98cc4570a337fbf89d
ravi.m85@gmail.com <ravi.m85@gmail.com@0177215c-dcb8-4386-07b5-442ffd92dceb>
2011-05-02 10:19:45 +0000

    script fixes

--- trunk/pa3/2node-test/2node-test.sh
+++ trunk/pa3/2node-test/2node-test.sh
@@ -2,5 +2,5 @@
 
 BASEDIR="~/cs550/pa3/"
 
-ssh compute-0-0 "$BASEDIR/obtain_server $BASEDIR/2node-test/compute-0-0.peers $BASEDIR/data/share0" &
-ssh compute-0-1 "$BASEDIR/obtain_server $BASEDIR/2node-test/compute-0-1.peers $BASEDIR/data/share1" &
+ssh compute-0-0 "$BASEDIR/obtain_server $@ $BASEDIR/2node-test/compute-0-0.peers $BASEDIR/data/share0 > /tmp/2node-test2.0.log" &
+ssh compute-0-1 "$BASEDIR/obtain_server $@ $BASEDIR/2node-test/compute-0-1.peers $BASEDIR/data/share1 > /tmp/2node-test2.1.log" &
--- trunk/pa3/relay-test/relay-test.sh
+++ trunk/pa3/relay-test/relay-test.sh
@@ -2,7 +2,7 @@
 
 BASEDIR="~/cs550/pa3/"
 
-ssh compute-0-0 "$BASEDIR/obtain_server $@ $BASEDIR/relay-test/compute-0-0.peers $BASEDIR/data/share0" &
-ssh compute-0-1 "$BASEDIR/obtain_server $@ $BASEDIR/relay-test/compute-0-1.peers $BASEDIR/data/share1" &
-ssh compute-0-2 "$BASEDIR/obtain_server $@ $BASEDIR/relay-test/compute-0-2.peers $BASEDIR/data/share2" &
-ssh compute-0-3 "$BASEDIR/obtain_server $@ $BASEDIR/relay-test/compute-0-3.peers $BASEDIR/data/share3" &
+ssh compute-0-0 "$BASEDIR/obtain_server $@ $BASEDIR/relay-test/compute-0-0.peers $BASEDIR/data/share0 > /tmp/relay-test.4.0.log" &
+ssh compute-0-1 "$BASEDIR/obtain_server $@ $BASEDIR/relay-test/compute-0-1.peers $BASEDIR/data/share1 > /tmp/relay-test.4.1.log" &
+ssh compute-0-2 "$BASEDIR/obtain_server $@ $BASEDIR/relay-test/compute-0-2.peers $BASEDIR/data/share2 > /tmp/relay-test.4.2.log" &
+ssh compute-0-3 "$BASEDIR/obtain_server $@ $BASEDIR/relay-test/compute-0-3.peers $BASEDIR/data/share3 > /tmp/relay-test.4.3.log" &
