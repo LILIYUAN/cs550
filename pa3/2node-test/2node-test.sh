a83502658d797287e1675dd79cf3fecd1a9939bf
ravi.m85 <ravi.m85@gmail.com>
2011-05-02 06:21:54 +0000

    fixed scripts

--- pa3/2node-test/2node-test.sh
+++ pa3/2node-test/2node-test.sh
@@ -2,5 +2,5 @@
 
 BASEDIR="~/cs550/pa3/"
 
-ssh compute-0-0 "$BASEDIR/obtain_server $BASEDIR/relay-test/compute-0-0.peers $BASEDIR/data/share0" &
-ssh compute-0-1 "$BASEDIR/obtain_server $BASEDIR/relay-test/compute-0-1.peers $BASEDIR/data/share1" &
+ssh compute-0-0 "$BASEDIR/obtain_server $BASEDIR/2node-test/compute-0-0.peers $BASEDIR/data/share0" &
+ssh compute-0-1 "$BASEDIR/obtain_server $BASEDIR/2node-test/compute-0-1.peers $BASEDIR/data/share1" &
